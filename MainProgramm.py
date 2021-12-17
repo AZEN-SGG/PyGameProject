2f004d641e30f3067c06366c9ea15cb4b04cf538
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2021-12-17 14:00:16 +0300

    Протестировал программу, встроил в неё иконку

--- MainProgramm.py
+++ MainProgramm.py
@@ -3,7 +3,55 @@ import os
 
 WIDTH = 650
 HEIGHT = 800
+FPS = 60
 
 game_folder = os.path.dirname(__file__)
 data_folder = os.path.join(game_folder, 'data')
 
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_image
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Across The Road")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player_image = pygame.image.load(os.path.join(data_folder, 'bigger_player.png')).convert()
+player = Player()
+all_sprites.add(player)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+
+    # Рендеринг
+    screen.fill('black')
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
