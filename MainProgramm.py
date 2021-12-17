6b724ef2db88fbc7b5dfd1c77df77ca8c7ae4cae
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2021-12-18 00:04:42 +0300

    Создал класс Board Реализовал нажатие на кнопку

--- MainProgramm.py
+++ MainProgramm.py
@@ -3,40 +3,80 @@ import os
 
 WIDTH = 650
 HEIGHT = 650
-FPS = 60
+FPS = 30
 
 game_folder = os.path.dirname(__file__)
 data_folder = os.path.join(game_folder, 'data')
+
+
+class Board:
+    def render(self, screen):
+        for y in range(HEIGHT):
+            for x in range(WIDTH):
+                pygame.draw.rect(screen, 'white',
+                                 (x * 50, y * 50, 50, 50), 1)
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = player_image
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.image.set_colorkey('white')
 
-    def render(self, screen):
-        for j in range(HEIGHT):
-            for i in range(WIDTH):
-                pygame.draw.rect(screen, 'white',
-                                 (i * 50, j * 50, 50, 50), 1)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT - 25)
+
+        print(self.rect.y)
+        print(self.rect.x)
 
     def update(self):
-        self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+        pass
+
+    def go_up(self):
+        if self.rect.y == 0:
+            pass
+
+        else:
+            self.rect.y -= 50
+
+    def go_down(self):
+        if self.rect.y == HEIGHT - 50:
+            pass
+
+        else:
+            self.rect.y += 50
+
+    def go_right(self):
+        if self.rect.x == WIDTH - 50:
+            self.rect.x = 0
+
+        else:
+            self.rect.x += 50
+
+    def go_left(self):
+        if self.rect.x == 0:
+            self.rect.x = WIDTH - 50
+
+        else:
+            self.rect.x -= 50
 
 
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Across The Road")
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
 clock = pygame.time.Clock()
+
 all_sprites = pygame.sprite.Group()
+
+board = Board()
+
 player_image = pygame.image.load(os.path.join(data_folder, 'bigger_player.png')).convert()
 player = Player()
+
 all_sprites.add(player)
 
 # Цикл игры
@@ -50,14 +90,28 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                player.go_up()
+
+            elif event.key == pygame.K_s:
+                player.go_down()
+
+            elif event.key == pygame.K_d:
+                player.go_right()
+
+            elif event.key == pygame.K_a:
+                player.go_left()
+
+    screen.fill((222, 184, 135))
     # Обновление
     all_sprites.update()
 
     # Рендеринг
-    screen.fill('black')
     all_sprites.draw(screen)
     # Вывод клетчатого поля
-    player.render(screen)
+    board.render(screen)
+
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
