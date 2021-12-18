5c37222ae1d314bb034e367049ec339dd9566c7a
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2021-12-18 21:16:14 +0300

    Добавил функцию add_sprite которая добавляет спрайты и их координаты Написал функцию return_back возвращает всех спрайтов на месте Перекати поел теперь крутится, игрок теперь поворачивается в сторону куда идёт Поменял цвет текста при проигрыше на красный Теперь при проигрыше все персонажи возвращаются назад Добавил в перекати поел настройку скорости

--- MainProgramm.py
+++ MainProgramm.py
@@ -17,14 +17,26 @@ def faced():  # Отображает надпись и завершает про
     faced_bool = True
 
     font = pygame.font.Font(None, 50)
-    text = font.render("Игра окончена", True, (100, 255, 100))  # Нужно дописать про пробел
+    text = font.render("Игра окончена", True, (255, 0, 0))  # Нужно дописать про пробел
     text_x = WIDTH // 2 - text.get_width() // 2
     text_y = HEIGHT // 2 - text.get_height() // 2
     text_w = text.get_width()
     text_h = text.get_height()
     screen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+    pygame.draw.rect(screen, (255, 0, 0), (text_x - 10, text_y - 10,
                                            text_w + 20, text_h + 20), 1)
+
+
+def add_sprite(sprite):
+    all_sprites.add(sprite)
+    sprites.append(sprite)
+    coordinats.append(sprite.COORDINATS)
+
+
+def return_back():
+    for sprite in range(len(sprites)):
+        sprites[sprite].rect.x = coordinats[sprite][0]
+        sprites[sprite].rect.y = coordinats[sprite][1]
 
 
 class Board:
@@ -41,20 +53,28 @@ class Player(pygame.sprite.Sprite):
         self.image = player_image
         self.image.set_colorkey('green')
 
+        self.COORDINATS = (300, 600)
+
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT - 25)
+        self.rect.center = WIDTH / 2, HEIGHT - 25
 
         print(self.rect.y)
         print(self.rect.x)
 
     def change_rect(self):
-        self.rect.y = HEIGHT - 50
-        self.rect.x = WIDTH // 2
+        self.image = player_image
+        self.image.set_colorkey('green')
+
+        self.rect.y = 600
+        self.rect.x = 300
 
     def update(self):
         pass
 
     def go_up(self):
+        self.image = player_image
+        self.image.set_colorkey('green')
+
         if self.rect.y == 0:
             pass
 
@@ -62,6 +82,9 @@ class Player(pygame.sprite.Sprite):
             self.rect.y -= 50
 
     def go_down(self):
+        self.image = player_back_image
+        self.image.set_colorkey('green')
+
         if self.rect.y == HEIGHT - 50:
             pass
 
@@ -69,14 +92,20 @@ class Player(pygame.sprite.Sprite):
             self.rect.y += 50
 
     def go_right(self):
-        if self.rect.x == WIDTH - 50:
+        self.image = player_right_image
+        self.image.set_colorkey('green')
+
+        if self.rect.x >= WIDTH - 50:
             self.rect.x = 0
 
         else:
             self.rect.x += 50
 
     def go_left(self):
-        if self.rect.x == 0:
+        self.image = player_left_image
+        self.image.set_colorkey('green')
+
+        if self.rect.x <= 0:
             self.rect.x = WIDTH - 50
 
         else:
@@ -84,15 +113,23 @@ class Player(pygame.sprite.Sprite):
 
 
 class Tumbleweed(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, DIRECTION='Right', SPEED=0):
         pygame.sprite.Sprite.__init__(self)
         self.image = tumbleweed_image
         self.image.set_colorkey('white')
 
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.COORDINATS = (x - 25, y - 25)
+        self.DIRECTION = DIRECTION
+        self.status = 1
 
-        self.speed = random.randint(5, 7)
+        self.rect = self.image.get_rect()
+        self.rect.center = x, y
+
+        if SPEED == 0:
+            self.SPEED = random.randint(5, 7)
+
+        else:
+            self.SPEED = SPEED
 
     def update(self):
         if pygame.sprite.collide_mask(self, player):
@@ -102,7 +139,31 @@ class Tumbleweed(pygame.sprite.Sprite):
             self.rect.x = -50
 
         else:
-            self.rect.x += self.speed
+            self.rect.x += self.SPEED
+
+        if self.status == 1:
+            self.image = tumbleweed_left_image
+            self.image.set_colorkey('white')
+
+            self.status = 2
+
+        elif self.status == 2:
+            self.image = tumbleweed_back_image
+            self.image.set_colorkey('white')
+
+            self.status = 3
+
+        elif self.status == 3:
+            self.image = tumbleweed_right_image
+            self.image.set_colorkey('white')
+
+            self.status = 4
+
+        else:
+            self.image = tumbleweed_image
+            self.image.set_colorkey('white')
+
+            self.status = 1
 
 
 class Bear(pygame.sprite.Sprite):
@@ -115,9 +176,10 @@ class Bear(pygame.sprite.Sprite):
 
         self.status = 1
         self.DIRECTION = DIRECTION
+        self.COORDINATS = (x - 25, y - 25)
 
         self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect.center = x, y
 
         if SPEED == 0:
             self.SPEED = random.randint(5, 7)
@@ -177,23 +239,31 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+sprites = []
+coordinats = []  # В этой переменной содержаться координаты всех персонажей
 
 player_image = pygame.image.load(os.path.join(data_folder, 'bigger_player.png')).convert()
+player_left_image = pygame.image.load(os.path.join(data_folder, 'bigger_left_player.png')).convert()
+player_right_image = pygame.image.load(os.path.join(data_folder, 'bigger_right_player.png')).convert()
+player_back_image = pygame.image.load(os.path.join(data_folder, 'bigger_back_player.png')).convert()
 player = Player()
 
 tumbleweed_image = pygame.image.load(os.path.join(data_folder, 'tumbleweed.png')).convert()
+tumbleweed_left_image = pygame.image.load(os.path.join(data_folder, 'tumbleweed_left.png')).convert()
+tumbleweed_back_image = pygame.image.load(os.path.join(data_folder, 'tumbleweed_back.png')).convert()
+tumbleweed_right_image = pygame.image.load(os.path.join(data_folder, 'tumbleweed_right.png')).convert()
 tumbleweed = Tumbleweed(0, HEIGHT - 175)
-tumbleweed2 = Tumbleweed(0, HEIGHT - 275)
+second_tumbleweed = Tumbleweed(0, HEIGHT - 275)
 
 bear_stand_image = pygame.image.load(os.path.join(data_folder, 'bear_go.png')).convert()
 bear_go_image = pygame.image.load(os.path.join(data_folder, 'bear_stand.png')).convert()
 bear_back_image = pygame.image.load(os.path.join(data_folder, 'bear_back.png')).convert()
-bear = Bear(WIDTH, HEIGHT - 375)
+bear = Bear(0, HEIGHT - 375)
 
-all_sprites.add(player)
-all_sprites.add(tumbleweed)
-all_sprites.add(tumbleweed2)
-all_sprites.add(bear)
+add_sprite(player)
+add_sprite(tumbleweed)
+add_sprite(second_tumbleweed)
+add_sprite(bear)
 
 # Цикл игры
 running = True
@@ -220,22 +290,22 @@ while running:
                 elif event.key == pygame.K_a:
                     player.go_left()
 
-                # elif event.key == pygame.K_SPACE:
-                # player.change_rect()
-
             else:
                 if event.key == pygame.K_SPACE:
-                    player.change_rect()
+                    return_back()
                     faced_bool = False
+
+                else:
+                    faced()
 
     # Обновление
     screen.fill((222, 184, 135))
 
-    # if not faced_bool:
-    all_sprites.update()
+    if not faced_bool:
+        all_sprites.update()
 
-    # else:
-    # faced()
+    else:
+        faced()
 
     # Рендеринг
     all_sprites.draw(screen)
