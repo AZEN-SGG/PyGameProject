363195469e382cc7694178cf8c8913a5a1df96a3
Margosha5 <94627318+Margosha5@users.noreply.github.com>
2022-01-05 13:33:32 +0300

    Добавила реализацию анимации с акулами, создала туман (пока что закоментирован), изменила картинку у аквалангиста, добавила класс для акул

--- SecondLevel.py
+++ SecondLevel.py
@@ -1,20 +1,95 @@
 import pygame
 import os
+import sys
 
 WIDTH = 650
 HEIGHT = 650
-FPS = 30
+FPS = 20
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 game_folder = os.path.dirname(__file__)
 data_folder = os.path.join(game_folder, 'data')
 
 
+def load_image(name, color_key=None):
+    fullname = os.path.join(name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_level(filename):
+    filename = filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+class Shark(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, SPEED, status):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.SPEED = SPEED
+        self.status = status
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+        self.image.set_colorkey('red')
+        if self.status == 1:
+            self.rect.y += self.SPEED
+            if self.rect.y == HEIGHT:
+                self.rect.y = -50
+        if self.status == 3:
+            self.rect.y -= self.SPEED
+            if self.rect.y == -50:
+                self.rect.y = HEIGHT
+
+
 class Board:
-    def render(self, screen):
-        for y in range(HEIGHT):
-            for x in range(WIDTH):
-                pygame.draw.rect(screen, 'white',
-                                 (x * 50, y * 50, 50, 50), 1)
+    def render(self, screen, coor):
+        j, i = coor
+        j //= 50
+        i //= 50
+        for y in range(HEIGHT // 50):
+            for x in range(WIDTH // 50):
+                if not (abs(y - j) <= 1 and abs(x - i) <= 1):
+                    pygame.draw.rect(screen, 'black',
+                                     (x * 50, y * 50, 50, 50))
 
 
 class Player(pygame.sprite.Sprite):
@@ -24,7 +99,10 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey('white')
 
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT - 25)
+        self.rect.center = WIDTH / 2, HEIGHT - 25
+
+    def get_rects(self):
+        return self.rect.y, self.rect.x
 
     def update(self):
         pass
@@ -75,7 +153,10 @@ player_image = pygame.image.load(os.path.join(data_folder, 'aqualunger.png')).co
 player = Player()
 
 all_sprites.add(player)
-
+shark1 = Shark(load_image("data/" + "shark_up.png"), 4, 1, WIDTH - 50, HEIGHT - 100, 5, 3)
+shark2 = Shark(load_image("data/" + "shark_down.png"), 4, 1, 0, 100, 5, 1)
+shark3 = Shark(load_image("data/" + "shark_down.png"), 4, 1, 0, 300, 5, 1)
+shark4 = Shark(load_image("data/" + "shark_up.png"), 4, 1, WIDTH - 50, 100, 5, 3)
 # Цикл игры
 running = True
 while running:
@@ -106,8 +187,7 @@ while running:
 
     # Рендеринг
     all_sprites.draw(screen)
-
-
+    # board.render(screen, player.get_rects())
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
