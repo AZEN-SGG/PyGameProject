7fbe1a52bb45b09a970b3e2fc49dac9efa385ffc
Margosha5 <94627318+Margosha5@users.noreply.github.com>
2022-01-05 16:11:43 +0300

    Изображение аквалангиста поворачивается при движение, акулы двигаются по кругу, добавлено изображение коралла

--- SecondLevel.py
+++ SecondLevel.py
@@ -12,6 +12,22 @@ player_group = pygame.sprite.Group()
 
 game_folder = os.path.dirname(__file__)
 data_folder = os.path.join(game_folder, 'data')
+faced_bool = False
+
+
+def faced():  # Отображает надпись и завершает программу
+    global faced_bool
+    faced_bool = True
+
+    font = pygame.font.Font(None, 50)
+    text = font.render("Игра окончена", True, (255, 0, 0))  # Нужно дописать про пробел
+    text_x = WIDTH // 2 - text.get_width() // 2
+    text_y = HEIGHT // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (255, 0, 0), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
 
 
 def load_image(name, color_key=None):
@@ -53,6 +69,7 @@ class Shark(pygame.sprite.Sprite):
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
+        self.image.set_colorkey('red')
         self.rect = self.rect.move(x, y)
         self.SPEED = SPEED
         self.status = status
@@ -72,12 +89,22 @@ class Shark(pygame.sprite.Sprite):
         self.image.set_colorkey('red')
         if self.status == 1:
             self.rect.y += self.SPEED
-            if self.rect.y == HEIGHT:
-                self.rect.y = -50
+            if self.rect.y == HEIGHT - 150:
+                self.__init__(load_image("data/" + "shark_right.png"), 2, 1, 0, HEIGHT - 100, 5, 4)
         if self.status == 3:
             self.rect.y -= self.SPEED
-            if self.rect.y == -50:
-                self.rect.y = HEIGHT
+            if self.rect.y == 50:
+                self.__init__(load_image("data/" + "shark_left.png"), 2, 1, WIDTH - 75, 0, 5, 2)
+        if self.status == 2:
+            self.rect.x -= self.SPEED
+            if self.rect.x == 0:
+                self.__init__(load_image("data/" + "shark_down.png"), 4, 1, 0, 0, 5, 1)
+        if self.status == 4:
+            self.rect.x += self.SPEED
+            if self.rect.x == WIDTH - 100:
+                self.__init__(load_image("data/" + "shark_up.png"), 4, 1, WIDTH - 50, HEIGHT - 150, 5, 3)
+        if pygame.sprite.collide_mask(self, player):
+            faced()
 
 
 class Board:
@@ -108,6 +135,8 @@ class Player(pygame.sprite.Sprite):
         pass
 
     def go_up(self):
+        self.image=player_image
+        self.image.set_colorkey('white')
         if self.rect.y == 0:
             pass
 
@@ -115,6 +144,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.y -= 50
 
     def go_down(self):
+        self.image = player_down_image
+        self.image.set_colorkey('white')
         if self.rect.y == HEIGHT - 50:
             pass
 
@@ -122,6 +153,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.y += 50
 
     def go_right(self):
+        self.image = player_right_image
+        self.image.set_colorkey('white')
         if self.rect.x == WIDTH - 50:
             self.rect.x = 0
 
@@ -129,6 +162,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += 50
 
     def go_left(self):
+        self.image = player_left_image
+        self.image.set_colorkey('white')
         if self.rect.x == 0:
             self.rect.x = WIDTH - 50
 
@@ -150,13 +185,18 @@ all_sprites = pygame.sprite.Group()
 board = Board()
 
 player_image = pygame.image.load(os.path.join(data_folder, 'aqualunger.png')).convert()
+player_left_image = pygame.image.load(os.path.join(data_folder, 'aqualunger_left.png')).convert()
+player_right_image = pygame.image.load(os.path.join(data_folder, 'aqualunger_right.png')).convert()
+player_down_image = pygame.image.load(os.path.join(data_folder, 'aqualunger_down.png')).convert()
 player = Player()
 
 all_sprites.add(player)
-shark1 = Shark(load_image("data/" + "shark_up.png"), 4, 1, WIDTH - 50, HEIGHT - 100, 5, 3)
+shark1 = Shark(load_image("data/" + "shark_up.png"), 4, 1, WIDTH - 50, HEIGHT - 300, 5, 3)
 shark2 = Shark(load_image("data/" + "shark_down.png"), 4, 1, 0, 100, 5, 1)
-shark3 = Shark(load_image("data/" + "shark_down.png"), 4, 1, 0, 300, 5, 1)
+shark3 = Shark(load_image("data/" + "shark_down.png"), 4, 1, 0, 400, 5, 1)
 shark4 = Shark(load_image("data/" + "shark_up.png"), 4, 1, WIDTH - 50, 100, 5, 3)
+shark5 = Shark(load_image("data/" + "shark_left.png"), 2, 1, 200, 0, 5, 2)
+shark6 = Shark(load_image("data/" + "shark_right.png"), 2, 1, 300, HEIGHT - 100, 5, 4)
 # Цикл игры
 running = True
 while running:
@@ -183,11 +223,12 @@ while running:
 
     screen.fill((0, 0, 139))
     # Обновление
-    all_sprites.update()
-
-    # Рендеринг
+    if not faced_bool:
+        all_sprites.update()
     all_sprites.draw(screen)
-    # board.render(screen, player.get_rects())
+    #board.render(screen, player.get_rects())
+    if faced_bool:
+        faced()
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
