6b45fa8eb07765aa00eb7cc867f0985e2fa51538
Катя <ekaterina030305@icloud.com>
2022-01-15 17:29:43 +0300

    Добавила огонь

--- Tirdlevel.py
+++ Tirdlevel.py
@@ -7,12 +7,33 @@ FPS = 30
 game_folder = os.path.dirname(__file__)
 data_folder = os.path.join(game_folder, 'data')
 
+matrix = [['' for _ in range(15)] for i in range(13)]
+
+
+def load_image(name, color_key=None):  # Функция для получения фотографий
+    fullname = os.path.join(name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 class Board:
     def render(self, screen):
         for y in range(HEIGHT):
             for x in range(WIDTH):
                 pygame.draw.rect(screen, 'white',
                                  (x * 50, y * 50, 50, 50), 1)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -54,6 +75,28 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 50
 
 
+class Flame(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        matrix[y // 50][x // 50] = 'Flame'
+        self.frames = []
+        self.cur_frame = 0
+        self.cut_sheet(sheet, columns, rows)
+        self.image = self.frames[self.cur_frame]
+        self.image.set_colorkey('white')
+        self.rect = self.image.get_rect()
+        self.rect.center = x, y
+
+    def cut_sheet(self, sheet, columns, rows):  # получение изображения
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+
 pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Across The Road")
@@ -61,13 +104,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 board = Board()
+
 player_image = pygame.image.load(os.path.join(data_folder, 'shlapa.png')).convert()
 player = Player()
 
-
+flame_image = pygame.image.load(os.path.join(data_folder, 'flame.png')).convert()
+flame1 = Flame(load_image("data/" + "flame.png"), 4, 1, 75, 75)
 
 all_sprites.add(player)
-
 
 # Цикл игры
 running = True
@@ -91,12 +135,10 @@ while running:
     screen.fill((123, 34, 52))
     # Обновление
     all_sprites.update()
-    board.render(screen)
 
     # Рендеринг
     all_sprites.draw(screen)
     # Вывод клетчатого поля
-    board.render(screen)
 
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
