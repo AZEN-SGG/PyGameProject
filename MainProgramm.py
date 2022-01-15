0012ee61cf197f58630d5a76d5ec7a38debeb2cb
Margosha5 <94627318+Margosha5@users.noreply.github.com>
2022-01-15 16:03:24 +0300

    Добавила основное окно с правилами(их нужно будет прописать)

--- MainProgramm.py
+++ MainProgramm.py
@@ -1,5 +1,71 @@
 import FirstLevel
 import SecondLevel
+import pygame
+import sys
+import os
 
+WIDTH = 750
+HEIGHT = 650
+FPS = 20
+
+
+def load_image(name, color_key=None):
+    name="data/" +name
+    fullname = os.path.join(name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Правила игры",
+                  "#to do: Написать правила"]
+
+    fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+pygame.init()
+pygame.mixer.init()
+pygame.display.set_caption("Adventure experience")
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+start_screen()
 SecondLevel.second_level(True)
 FirstLevel.first_level(True)
